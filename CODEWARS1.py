60ed803f215ac2cd37b49224a6870af2dd4a4753
Brian Dorr <Jeeves2006@gmail.com>
2024-11-10 04:09:29 -0700

    added comments

--- CODEWARS1.py
+++ CODEWARS1.py
@@ -1,6 +1,6 @@
 # Even or odd
-def even_or_odd(number):
-    if number % 2 == 0:
+def even_or_odd(number): # defines a function and takes one parameter (number)
+    if number % 2 == 0: # divides the number by 2, if it equals 0 it's even
         return "Even"
     else:
         return "Odd"
@@ -11,7 +11,7 @@ def number_to_string(num):
     return str_num
 
 #Vowel count
-def get_count(string):
-    vowels = "a,e,i,o,u"
-    count = sum(1 for char in string if char in vowels)
+def get_count(string): # function that counts vowels from the sting input
+    vowels = "a,e,i,o,u" # defines the vowels
+    count = sum(1 for char in string if char in vowels) # for every vowel found in the string it's given a 1, then the (sum) is added up
     return count
